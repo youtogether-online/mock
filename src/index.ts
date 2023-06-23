611665e3545e85b708d7a119a6450428db1d760a
frkam <gearboxop@gmail.com>
2023-06-23 16:54:37 +0300

    feat: add auth email and email send code requests

--- src/index.ts
+++ src/index.ts
@@ -17,28 +17,30 @@ app.use(express.text());
 app.use(cookieParser());
 const cookieValue = "abcd";
 
-const getSession =
-  (): components["responses"]["SessionGetSuccess"]["content"]["application/json"] => {
+const cookieOptions = {
+  httpOnly: true,
+  sameSite: "none" as const,
+  maxAge: 1000000,
+  secure: true,
+};
+
+app.get("/api/auth/session", (req: Request, res: Response) => {
+  if (req.cookies.session_id === cookieValue) {
     const Session: components["schemas"]["Me"] = {
       createTime: new Date(faker.date.anytime().toString()).toISOString(),
       email: faker.internet.email(),
-      friendsIds: [12],
+      friendsIds: [12, 24],
       isEmailVerified: faker.datatype.boolean(),
       language: "EN",
       name: "user1234",
       firstName: faker.person.firstName(),
       lastName: faker.person.lastName(),
       role: "USER",
-      theme: "SYSTEM",
+      theme: "LIGHT",
       biography: faker.person.bio(),
     };
 
-    return Session;
-  };
-
-app.get("/api/auth/session", (req: Request, res: Response) => {
-  if (req.cookies.session_id === cookieValue) {
-    res.status(200).json(getSession());
+    res.status(200).json(Session);
   } else {
     res.status(401).json({
       description: "Unauthorized",
@@ -46,10 +48,25 @@ app.get("/api/auth/session", (req: Request, res: Response) => {
   }
 });
 
+app.post("/api/email/send-code", (req: Request, res: Response) => {
+  console.log("Code: 12345");
+  res.status(200).json();
+});
+
+app.post("/api/auth/email", (req: Request, res: Response) => {
+  if (req.body.email === "frkam@icloud.com" && req.body.code === "12345") {
+    res.status(201).cookie("session_id", cookieValue, cookieOptions).json();
+  } else {
+    res.status(400).json({
+      code: "code_invalid_or_expired",
+      description: "Code invalid or expired",
+    });
+  }
+});
+
 app.post("/api/auth/password", (req: Request, res: Response) => {
   if (req.body.password?.length < 8) {
-    res.status(400).json({
-      code: "invalid_validation",
+    res.status(422).json({
       fields: { password: "Password should contain at least 8 symbols" },
     });
   }
@@ -59,7 +76,7 @@ app.post("/api/auth/password", (req: Request, res: Response) => {
     req.body.password === "12345678910"
   ) {
     res
-      .status(200)
+      .status(201)
       .cookie("session_id", cookieValue, {
         httpOnly: true,
         sameSite: "none",
